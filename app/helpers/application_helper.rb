43eecc843298b3a8c44332d6e7538cdee5ce509e
Justin Cheng <jccccf@gmail.com>
2013-01-18 11:25:24 -0800

    An attempt at scene descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,20 @@
 module ApplicationHelper
 
   def parse_story(text)
-    parsed = text.split("{{p}}").join("\n\n")
-    markdown parsed
+    parsed = text.split("{{p}}").map do |para|
+      if para.include? "{{s}}"
+        parts = para.split("{{s}}")
+        if parts[0].size > 0
+          "### " + parts[0] + "\n\n" + parts[1]
+        else
+          parts[1]
+        end
+      else
+        para
+      end
+    end
+
+    markdown(parsed.join("\n\n"))
   end
 
   def markdown(text)
