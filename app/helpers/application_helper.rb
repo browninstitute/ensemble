275e28db07381ed6bc7a6634de1a0c197f19e36c
Justin Cheng <jccccf@gmail.com>
2013-01-23 01:53:21 -0800

    Updated New, Edit, View to use Scene and Paragraph Models.
    Drafts and Versions are now probably completely broken.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,2 @@
 module ApplicationHelper
-
-  def parse_story(text)
-    parts = []
-    parsed = text.split("{{p}}").map do |para|
-      next if para.size == 0
-      part = { :para => "", :scene => "" }
-      if para.include? "{{s}}"
-        para_parts = para.split("{{s}}")
-        if para_parts[0].size > 0
-          part[:scene] = markdown(para_parts[0])
-        end
-        part[:para] = markdown(para_parts[1])
-      else
-        part[:para] = markdown(para)
-      end
-      parts << part
-    end
-    parts
-  end
-
-  def markdown(text)
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true).render(text).html_safe
-  end
-
 end
