64e9c8b42c402883a6e448a8b3e595c36504fbe1
Joy Kim <jojo080889@gmail.com>
2013-01-23 23:40:22 -0800

    delete contributions

--- app/controllers/paragraphs_controller.rb
+++ app/controllers/paragraphs_controller.rb
@@ -2,6 +2,7 @@ class ParagraphsController < ApplicationController
   def create
     @scene = Scene.find(params[:scene_id])
     @p = @scene.paragraphs.build(params[:paragraph])
+    @p.user_id = current_user.id
     if @p.save
       redirect_to @p.scene.story
     else
@@ -9,5 +10,12 @@ class ParagraphsController < ApplicationController
       @story = @scene.story
       render 'stories/show'
     end
+  end
+
+  def destroy
+    @p = Paragraph.find(params[:id])
+    @p.destroy
+
+    redirect_to @p.scene.story
   end
 end
