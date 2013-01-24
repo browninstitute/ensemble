6ffe0e71c4a639ad3ec53f45be733b5fa28e860f
Joy Kim <jojo080889@gmail.com>
2013-01-23 22:27:40 -0800

    validates that content exists when submitting a paragraph

--- app/controllers/paragraphs_controller.rb
+++ app/controllers/paragraphs_controller.rb
@@ -4,6 +4,10 @@ class ParagraphsController < ApplicationController
     @p = @scene.paragraphs.build(params[:paragraph])
     if @p.save
       redirect_to @p.scene.story
+    else
+      flash[:error] = "Your submitted answer cannot be empty. Please type something and then submit."
+      @story = @scene.story
+      render 'stories/show'
     end
   end
 end
--- app/models/paragraph.rb
+++ app/models/paragraph.rb
@@ -3,4 +3,5 @@ class Paragraph < ActiveRecord::Base
   belongs_to :scene
   acts_as_list :scope => :scene
   attr_accessible :content, :position, :title
+  validates :content, :presence => true
 end
