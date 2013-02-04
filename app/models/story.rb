dde048097f2d178cdb4e7f560fec0efeff37c922
Joy Kim <jojo080889@gmail.com>
2013-02-03 20:20:28 -0800

    fixed bug where edit story background was interfering with form

--- app/models/story.rb
+++ app/models/story.rb
@@ -38,57 +38,61 @@ class Story < ActiveRecord::Base
   # Update the contents of this story (scenes and paragraphs),
   # based on JSON generated in _paragraphs.html.erb
   def update_contents(content_json)
-    content = ActiveSupport::JSON.decode(content_json)
+    Scene.transaction do
+      Paragraph.transaction do
+        content = ActiveSupport::JSON.decode(content_json)
 
-    # Ensure that all scene ids and paragraph ids belong to this story
-    content.each do |scene|
-      if scene.include? 'id'
-        s = Scene.find(scene['id'].to_i)
-        raise "Scene Doesn't Belong to Story" if s.story != self
-        scene['paragraphs'].each do |paragraph|
-          if paragraph.include? 'id'
-            p = Paragraph.find(paragraph['id'].to_i)
-            raise "Paragraph Doesn't Belong to Scene" if p.scene != s
+        # Ensure that all scene ids and paragraph ids belong to this story
+        content.each do |scene|
+          if scene.include? 'id'
+            s = Scene.find(scene['id'].to_i)
+            raise "Scene Doesn't Belong to Story" if s.story != self
+            scene['paragraphs'].each do |paragraph|
+              if paragraph.include? 'id'
+                p = Paragraph.find(paragraph['id'].to_i)
+                raise "Paragraph Doesn't Belong to Scene" if p.scene != s
+              end
+            end
           end
         end
-      end
-    end
 
-    # Delete old scenes (cascades paragraph deletion)
-    # TODO place this under a transaction, so scenes/paragraphs aren't deleted if something
-    # fails later on
-    existing_scene_ids = Set.new(self.scenes.map { |scene| scene.id })
-    new_scene_ids = Set.new(content.map { |scene| (scene.include? 'id') ? scene['id'].to_i : 0 })
-    scenes_to_delete_ids = existing_scene_ids - new_scene_ids
-    scenes_to_delete_ids.each do |scene_id|
-      Scene.find(scene_id).destroy
-    end
+        debugger
 
-    # Update each scene, creating a new scene if no id is present
-    content.each do |scene|
-      scene_pos = scene['position'].to_i
-      scene_obj = if !scene.include? 'id'
-        scene_obj = self.scenes.create
-        scene_obj.insert_at(scene_pos)
-        scene_obj
-      else
-        Scene.find(scene['id'])
-      end
-      scene_obj.title = scene['title']
-      scene_obj.content = scene['content']
-      scene_obj.save!
-      
-      # Update each paragraph, creating a new paragraph attached to the scene if no id is present
-      scene['paragraphs'].each do |paragraph|
-        para_obj = if paragraph.include? 'id'
-          para_id = paragraph['id'].to_i
-          Paragraph.find(para_id)
-        else
-          scene_obj.paragraphs.create
+        # Delete old scenes (cascades paragraph deletion)
+        existing_scene_ids = Set.new(self.scenes.map { |scene| scene.id })
+        new_scene_ids = Set.new(content.map { |scene| (scene.include? 'id') ? scene['id'].to_i : 0 })
+        scenes_to_delete_ids = existing_scene_ids - new_scene_ids
+        scenes_to_delete_ids.each do |scene_id|
+          Scene.find(scene_id).destroy
         end
-        para_obj.content = paragraph['content']
-        para_obj.user_id = paragraph['user_id']
-        para_obj.save!
+
+        # Update each scene, creating a new scene if no id is present
+        content.each do |scene|
+          scene_pos = scene['position'].to_i
+          scene_obj = if !scene.include? 'id'
+            scene_obj = self.scenes.create
+            scene_obj.insert_at(scene_pos)
+            scene_obj
+          else
+            Scene.find(scene['id'])
+          end
+          scene_obj.title = scene['title']
+          scene_obj.content = scene['content']
+          scene_obj.save!
+          
+          # Update each paragraph, creating a new paragraph attached to the scene if no id is present
+          scene['paragraphs'].each do |paragraph|
+            para_obj = if paragraph.include? 'id'
+              para_id = paragraph['id'].to_i
+              Paragraph.find(para_id)
+            else
+              scene_obj.paragraphs.create
+            end
+            para_obj.content = paragraph['content']
+            para_obj.user_id = paragraph['user_id']
+            para_obj.save!
+          end
+        end
       end
     end
   end
