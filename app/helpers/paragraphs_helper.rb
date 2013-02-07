ad3bfe8ba3a15fbbecac891ffc7237b65e0c3734
Joy Kim <jojo080889@gmail.com>
2013-02-06 19:56:22 -0800

    fixed bug with callout rendering in edit mode

--- app/helpers/paragraphs_helper.rb
+++ app/helpers/paragraphs_helper.rb
@@ -1,11 +1,11 @@
 module ParagraphsHelper
   # Generates CSS classes for a paragraph depending on
   # the status of paragraphs in a scene.
-  def paragraphs_classes(p, scene)
+  def paragraphs_classes(p, scene, edit_mode = false)
     classes = ""
     classes += " multiple" if scene.paragraphs.length > 1
     classes += " unwritten" if scene.paragraphs.length < 1
-    classes += " open" if !scene.title.blank?
+    classes += " open" if !scene.title.blank? && !edit_mode
     classes
   end
 end
