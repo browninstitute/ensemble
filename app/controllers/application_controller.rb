ec713d9b01bbafed3712f309235ad5fe8723376f
Justin Cheng <jccccf@gmail.com>
2013-02-19 22:33:24 -0800

    Fixed sign in bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    if request.env['omniauth.origin']
+    if request.env['omniauth.origin'] && request.env['omniauth.origin'] != new_user_session_url
       request.env['omniauth.origin']
     else
       root_path
