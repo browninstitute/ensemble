69ff1e66b9c5f70e79d4c44c8c14aae1e9338a62
Joy Kim <jojo080889@gmail.com>
2013-03-03 12:47:27 -0800

    add moderator email preferences

--- app/models/notification_observer.rb
+++ app/models/notification_observer.rb
@@ -4,9 +4,17 @@ class NotificationObserver < ActiveRecord::Observer
   def after_create(record)
     if record.is_a? Comment
       commenter = User.find(record.user_id)
-      
+     
+      if !record.scene.nil? && !record.scene.story.nil?
+        mod = User.find(record.scene.story.user_id)
+        if mod.id != commenter.id && mod.settings['email.mod.comment_notification']
+          NotificationMailer.delay.mod_comment_notification(mod, commenter, record)
+          mod_comment_notification_sent = true
+        end
+      end
+
       record.scene.commenters.each do |noticee|
-        if commenter.id != noticee.id && noticee.settings['email.comment_notification']
+        if !(noticee.id == mod.id && mod_comment_notification_sent) && commenter.id != noticee.id && noticee.settings['email.comment_notification']
           NotificationMailer.delay.comment_notification(noticee, commenter, record)
         end
       end
@@ -19,8 +27,16 @@ class NotificationObserver < ActiveRecord::Observer
     elsif record.is_a? Paragraph
       contributor = User.find(record.user_id)
 
+      if !record.scene.nil? && !record.scene.story.nil?
+        mod = User.find(record.scene.story.user_id)
+        if mod.id != contributor.id && mod.settings['email.mod.paragraph_notification']
+          NotificationMailer.delay.mod_paragraph_notification(mod, contributor, record)
+          mod_para_notification_sent = true
+        end
+      end
+      
       record.scene.commenters.each do |noticee|
-        if contributor.id != noticee.id && noticee.settings['email.paragraph_notification']
+        if !(noticee.id == mod.id && mod_para_notification_sent) && contributor.id != noticee.id && noticee.settings['email.paragraph_notification']
           NotificationMailer.delay.paragraph_notification(noticee, contributor, record)
         end
       end
@@ -30,14 +46,48 @@ class NotificationObserver < ActiveRecord::Observer
         voter = User.find(record.whodunnit)
         paragraph = Paragraph.find(record.item_id)
         noticee = User.find(paragraph.user_id)
-        if voter.id != noticee.id && noticee.settings['email.like_notification']
+        
+        if !paragraph.scene.nil? && !paragraph.scene.story.nil?
+          mod = User.find(paragraph.scene.story.user_id)
+          if mod.id != voter.id && mod.settings['email.mod.like_notification']
+            NotificationMailer.delay.mod_like_notification(mod, voter, paragraph)
+            mod_like_notification_sent = true
+          end
+        end
+        
+        if !(noticee.id == mod.id && mod_like_notification_sent) && voter.id != noticee.id && noticee.settings['email.like_notification']
           NotificationMailer.delay.like_notification(noticee, voter, paragraph)
+        end
+      elsif record.item_type == "Paragraph" && record.event == "win"
+        marker = User.find(record.whodunnit)
+        paragraph = Paragraph.find(record.item_id)
+        noticee = User.find(paragraph.user_id)
+
+        if !paragraph.scene.nil? && !paragraph.scene.story.nil?
+          mod = User.find(paragraph.scene.story.user_id)
+          if mod.id != marker.id && mod.settings['email.mod.winner_notification']
+            NotificationMailer.delay.mod_winner_notification(mod, marker, paragraph)
+            mod_winner_notification_sent = true
+          end
+        end
+
+        if !(noticee.id == mod.id && mod_winner_notification_sent) && marker.id != noticee.id && noticee.settings['email.winner_notification']
+          NotificationMailer.delay.winner_notification(noticee, paragraph)
         end
       elsif record.item_type == "Paragraph" && record.event == "update"
         editor = User.find(record.whodunnit)
         paragraph = Paragraph.find(record.item_id)
         noticee = User.find(paragraph.user_id)
-        if editor.id != noticee.id && noticee.settings['email.update_paragraph_notification']
+        
+        if !paragraph.scene.nil? && !paragraph.scene.story.nil?
+          mod = User.find(paragraph.scene.story.user_id)
+          if mod.id != editor.id && mod.settings['email.mod.update_paragraph_notification']
+            NotificationMailer.delay.mod_update_paragraph_notification(mod, editor, paragraph)
+            mod_update_para_notification_sent = true
+          end
+        end
+        
+        if !(noticee.id == mod.id && mod_update_para_notification_sent) && editor.id != noticee.id && noticee.settings['email.update_paragraph_notification']
           NotificationMailer.delay.update_paragraph_notification(noticee, editor, paragraph)
         end
       elsif record.item_type == "Scene" && record.event == "update"
--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,8 +1,16 @@
 if Settings.table_exists? && User.table_exists? && !defined?(::Rake)
+  # Set setting defaults to story starters
+  User.settings['email.mod.comment_notification'] = true
+  User.settings['email.mod.paragraph_notification'] = true
+  User.settings['email.mod.update_paragraph_notification'] = false
+  User.settings['email.mod.like_notification'] = false
+  User.settings['email.mod.winner_notification'] = false
+  
   # Set setting defaults for users
   User.settings['email.comment_notification'] = true
   User.settings['email.comment_para_notification'] = false
   User.settings['email.like_notification'] = true
+  User.settings['email.winner_notification'] = true
   User.settings['email.paragraph_notification'] = true
   User.settings['email.update_paragraph_notification'] = true
   User.settings['email.update_scene_notification'] = true
