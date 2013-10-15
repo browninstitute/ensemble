1d10da50f6e161e930631cd69b787bab0bac1811
Joy Kim <jojo080889@gmail.com>
2013-10-15 15:45:11 -0700

    stories without arrowhead in the title don't have the arrowhead banner

--- lib/arrowhead.rb
+++ lib/arrowhead.rb
@@ -1,6 +1,6 @@
 module Arrowhead
   def self.is_arrowhead_story?(story)
-    ARROWHEAD_USERIDS.include? story.user_id
+    ARROWHEAD_USERIDS.include?(story.user_id) && story.title.include?("Arrowhead")
   end
 
   def self.is_arrowhead_author?(user_id)
