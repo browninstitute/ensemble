0b830e18bf63a467831b6c1f35170959807f0e9c
Joy Kim <jojo080889@gmail.com>
2014-02-08 22:11:41 -0800

    whoops, use new instead of create so guest user doesn't get created before we can turn off confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < ActionController::Base
   end
 
   def create_guest_user
-    u = User.create(:name => "guest", :email => "guest_#{Time.now.to_i}#{rand(99)}@example.com")
+    u = User.new(:name => "guest", :email => "guest_#{Time.now.to_i}#{rand(99)}@example.com")
     u.skip_confirmation!
     u.save!(:validate => false)
     session[:guest_user_id] = u.id
